2a51f6dcecdec09a2fcc7574e19830a68191e6d8
Huy Nguyen <i.love.to.smile.around@gmail.com>
2021-04-01 11:33:32 +0700

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
+	import { Button } from "carbon-components-svelte";
+	import { DataTable } from "carbon-components-svelte";
+</script>
+<script context="module">
+	export const ssr = false;
 </script>
 
 <main>
 	<h1>Hello world!</h1>
 
 	<Counter />
-
+	<Button>Primary button</Button>
+	<DataTable
+  headers={[{ key: 'name', value: 'Name' }, { key: 'protocol', value: 'Protocol' }, { key: 'port', value: 'Port' }, { key: 'rule', value: 'Rule' }]}
+  rows={[{ id: 'a', name: 'Load Balancer 3', protocol: 'HTTP', port: 3000, rule: 'Round robin' }, { id: 'b', name: 'Load Balancer 1', protocol: 'HTTP', port: 443, rule: 'Round robin' }, { id: 'c', name: 'Load Balancer 2', protocol: 'HTTP', port: 80, rule: 'DNS delegation' }, { id: 'd', name: 'Load Balancer 6', protocol: 'HTTP', port: 3000, rule: 'Round robin' }, { id: 'e', name: 'Load Balancer 4', protocol: 'HTTP', port: 443, rule: 'Round robin' }, { id: 'f', name: 'Load Balancer 5', protocol: 'HTTP', port: 80, rule: 'DNS delegation' }]}
+/>
 	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 </main>
 
