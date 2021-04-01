49ace165cf8042037f88726ab7ae008fef4d9956
Huy Nguyen <i.love.to.smile.around@gmail.com>
2021-04-01 12:22:08 +0700

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,54 +1,105 @@
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
-	import { Button } from "carbon-components-svelte";
-	import { DataTable } from "carbon-components-svelte";
+	import {
+    Header,
+    HeaderUtilities,
+    HeaderAction,
+    HeaderPanelLinks,
+    HeaderPanelDivider,
+    HeaderPanelLink,
+    SideNav,
+    SideNavItems,
+    SideNavMenu,
+    SideNavMenuItem,
+    SideNavLink,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+    TileGroup,
+    RadioTile,
+  } from "carbon-components-svelte";
+  import { expoIn } from "svelte/easing";
+
+  let isSideNavOpen = false;
+  let isOpen = false;
+  let selected = "0";
+  let transitions = {
+    "0": {
+      text: "Default (duration: 200ms)",
+      value: { duration: 200 },
+    },
+    "1": {
+      text: "Custom (duration: 600ms, delay: 50ms, easing: expoIn)",
+      value: { duration: 600, delay: 50, easing: expoIn },
+    },
+    "2": {
+      text: "Disabled",
+      value: false,
+    },
+  };
 </script>
 <script context="module">
 	export const ssr = false;
 </script>
 
+<Header company="Mixcore" platformName="CMS" bind:isSideNavOpen>
+	<div slot="skip-to-content">
+	  <SkipToContent />
+	</div>
+	<HeaderUtilities>
+	  <HeaderAction bind:isOpen transition={transitions[selected].value}>
+		<HeaderPanelLinks>
+		  <HeaderPanelDivider>Switcher subject 1</HeaderPanelDivider>
+		  <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+		  <HeaderPanelDivider>Switcher subject 2</HeaderPanelDivider>
+		  <HeaderPanelLink>Switcher item 1</HeaderPanelLink>
+		  <HeaderPanelLink>Switcher item 2</HeaderPanelLink>
+		  <HeaderPanelLink>Switcher item 3</HeaderPanelLink>
+		  <HeaderPanelLink>Switcher item 4</HeaderPanelLink>
+		  <HeaderPanelLink>Switcher item 5</HeaderPanelLink>
+		</HeaderPanelLinks>
+	  </HeaderAction>
+	</HeaderUtilities>
+  </Header>
+  
+  <SideNav bind:isOpen={isSideNavOpen}>
+	<SideNavItems>
+	  <SideNavLink text="Link 1" />
+	  <SideNavLink text="Link 2" />
+	  <SideNavLink text="Link 3" />
+	  <SideNavMenu text="Menu">
+		<SideNavMenuItem href="/" text="Link 1" />
+		<SideNavMenuItem href="/" text="Link 2" />
+		<SideNavMenuItem href="/" text="Link 3" />
+	  </SideNavMenu>
+	</SideNavItems>
+  </SideNav>
+  
+  <Content>
+	<Grid>
+	  <Row>
+		<Column>
+		  <h1>HeaderSwitcher</h1>
+		  <p>
+			Select a transition option below and click on the App Switcher icon in
+			the top right.
+		  </p>
+		  <TileGroup legend="App switcher transitions" bind:selected>
+			{#each Object.keys(transitions) as key}
+			  <RadioTile value={key}>{transitions[key].text}</RadioTile>
+			{/each}
+		  </TileGroup>
+
 <main>
 	<h1>Hello world!</h1>
 
 	<Counter />
-	<Button>Primary button</Button>
-	<DataTable
-  headers={[{ key: 'name', value: 'Name' }, { key: 'protocol', value: 'Protocol' }, { key: 'port', value: 'Port' }, { key: 'rule', value: 'Rule' }]}
-  rows={[{ id: 'a', name: 'Load Balancer 3', protocol: 'HTTP', port: 3000, rule: 'Round robin' }, { id: 'b', name: 'Load Balancer 1', protocol: 'HTTP', port: 443, rule: 'Round robin' }, { id: 'c', name: 'Load Balancer 2', protocol: 'HTTP', port: 80, rule: 'DNS delegation' }, { id: 'd', name: 'Load Balancer 6', protocol: 'HTTP', port: 3000, rule: 'Round robin' }, { id: 'e', name: 'Load Balancer 4', protocol: 'HTTP', port: 443, rule: 'Round robin' }, { id: 'f', name: 'Load Balancer 5', protocol: 'HTTP', port: 80, rule: 'DNS delegation' }]}
-/>
 	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 </main>
+		</Column>
+	  </Row>
+	</Grid>
+  </Content>
 
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
