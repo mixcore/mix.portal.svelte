17962544a20696128364a0a4a4530a2fe3f49edf
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-04-01 21:34:29 +0700

    upd

---
+++ src/routes/settings/$layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../../app.scss';
+</script>
+
+<div class="submenu">
+	<a href="/">Home</a>
+	<a href="/settings/profile">Profile</a>
+	<a href="/settings/notifications">Notifications</a>
+</div>
+
+<slot />
--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
 </script>
-
 <script context="module">
 	export const ssr = false;
 </script>
 
+
 <main>
-	<h1>Hello world!</h1>
+	<h1>Settings</h1>
 
 	<Counter />
+	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 </main>
-
+		
