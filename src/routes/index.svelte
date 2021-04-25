33a53084b364b8bfdc0819d092687ff0fa9d9df2
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-04-25 14:05:34 +0700

    Initial commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,2 @@
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-</script>
-
-<script context="module">
-	export const ssr = false;
-</script>
-
-<main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-</main>
-
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
