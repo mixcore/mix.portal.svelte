0d4e388e300728c58dd57621b2e0e6bc47551221
Si Nguyen <singuyen.it@gmail.com>
2021-05-04 14:18:45 +0700

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,13 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-    plugins: ['svelte3', '@typescript-eslint', 'o-autofix'],
+    plugins: ['svelte3', '@typescript-eslint', 'no-autofix'],
     ignorePatterns: ['*.cjs'],
     overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+    rules: {
+        "prefer-const": "off",
+        "no-autofix/prefer-const": "warn",
+    },
     settings: {
         'svelte3/typescript': require('typescript')
     },
@@ -17,8 +21,4 @@ module.exports = {
         es2017: true,
         node: true
     },
-    rules: {
-        "prefer-const": "off",
-        "no-autofix/prefer-const": "warn",
-    }
 };
