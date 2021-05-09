f328ff1fb8b59d66dbf5088cd7f71dc611e975b0
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-05-09 15:52:06 +0700

    upgrade kit

---
+++ .svelte-kit/dev/generated/manifest.js
@@ -0,0 +1,34 @@
+const c = [
+	() => import("..\\..\\..\\src\\routes\\__layout.svelte"),
+	() => import("..\\components\\error.svelte"),
+	() => import("..\\..\\..\\src\\routes\\index.svelte"),
+	() => import("..\\..\\..\\src\\routes\\website-builder\\__layout.svelte"),
+	() => import("..\\..\\..\\src\\routes\\website-builder\\index.svelte"),
+	() => import("..\\..\\..\\src\\routes\\ecommerce\\__layout.svelte"),
+	() => import("..\\..\\..\\src\\routes\\ecommerce\\index.svelte"),
+	() => import("..\\..\\..\\src\\routes\\blogs\\__layout.svelte"),
+	() => import("..\\..\\..\\src\\routes\\blogs\\index.svelte"),
+	() => import("..\\..\\..\\src\\routes\\blogs\\posts\\__layout.svelte"),
+	() => import("..\\..\\..\\src\\routes\\blogs\\posts\\index.svelte")
+];
+
+const d = decodeURIComponent;
+
+export const routes = [
+	// src/routes/index.svelte
+	[/^\/$/, [c[0], c[2]], [c[1]]],
+
+	// src/routes/website-builder/index.svelte
+	[/^\/website-builder\/?$/, [c[0], c[3], c[4]], [c[1]]],
+
+	// src/routes/ecommerce/index.svelte
+	[/^\/ecommerce\/?$/, [c[0], c[5], c[6]], [c[1]]],
+
+	// src/routes/blogs/index.svelte
+	[/^\/blogs\/?$/, [c[0], c[7], c[8]], [c[1]]],
+
+	// src/routes/blogs/posts/index.svelte
+	[/^\/blogs\/posts\/?$/, [c[0], c[7], c[9], c[10]], [c[1]]]
+];
+
+export const fallback = [c[0](), c[1]()];
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-const sveltePreprocess = require('svelte-preprocess');
+// const sveltePreprocess = require('svelte-preprocess');
+import sveltePreprocess from 'svelte-preprocess';
 //const node = require('@sveltejs/adapter-node');
-const static = require('@sveltejs/adapter-static');
-const pkg = require('./package.json');
+// const static = require('@sveltejs/adapter-static');
+import adapter from '@sveltejs/adapter-static';
+// const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: sveltePreprocess(),
@@ -12,7 +14,12 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: static(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
@@ -24,3 +31,5 @@ module.exports = {
 		// }
 	}
 };
+
+export default config;
