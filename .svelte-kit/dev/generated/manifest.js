9708a4cc8887883ad23856fb93495047f5a5696d
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-05-09 16:59:36 +0700

    add security route

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -6,6 +6,8 @@ const c = [
 	() => import("..\\..\\..\\src\\routes\\website-builder\\index.svelte"),
 	() => import("..\\..\\..\\src\\routes\\ecommerce\\__layout.svelte"),
 	() => import("..\\..\\..\\src\\routes\\ecommerce\\index.svelte"),
+	() => import("..\\..\\..\\src\\routes\\security\\__layout.svelte"),
+	() => import("..\\..\\..\\src\\routes\\security\\index.svelte"),
 	() => import("..\\..\\..\\src\\routes\\blogs\\__layout.svelte"),
 	() => import("..\\..\\..\\src\\routes\\blogs\\index.svelte"),
 	() => import("..\\..\\..\\src\\routes\\blogs\\posts\\__layout.svelte"),
@@ -24,11 +26,14 @@ export const routes = [
 	// src/routes/ecommerce/index.svelte
 	[/^\/ecommerce\/?$/, [c[0], c[5], c[6]], [c[1]]],
 
+	// src/routes/security/index.svelte
+	[/^\/security\/?$/, [c[0], c[7], c[8]], [c[1]]],
+
 	// src/routes/blogs/index.svelte
-	[/^\/blogs\/?$/, [c[0], c[7], c[8]], [c[1]]],
+	[/^\/blogs\/?$/, [c[0], c[9], c[10]], [c[1]]],
 
 	// src/routes/blogs/posts/index.svelte
-	[/^\/blogs\/posts\/?$/, [c[0], c[7], c[9], c[10]], [c[1]]]
+	[/^\/blogs\/posts\/?$/, [c[0], c[9], c[11], c[12]], [c[1]]]
 ];
 
 export const fallback = [c[0](), c[1]()];
---
+++ src/routes/security/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+<script context="module">
+	export const ssr = false;
+</script>
+
+
+<main>
+	<h1>Auth</h1>
+</main>
+		
