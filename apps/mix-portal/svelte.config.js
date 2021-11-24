a1d66940f23be157d98b0ed1160509cb3b554105
Huy Nguyen <Smilefounder@users.noreply.github.com>
2021-11-24 12:35:16 +0700

    fix ssr run fail

--- apps/mix-portal/svelte.config.js
+++ apps/mix-portal/svelte.config.js
@@ -3,17 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-  kit: {
-    vite: {
-      resolve: {
-        alias: {
-          "@mix.core/shared": path.resolve('../../libs/shared/src'),
-          "@mix.core/libs": path.resolve('../../libs/mix.lib.ts/src')
+    preprocess: preprocess(),
+    kit: {
+        ssr: false,
+        vite: {
+            resolve: {
+                alias: {
+                    "@mix.core/shared": path.resolve('../../libs/shared/src'),
+                    "@mix.core/libs": path.resolve('../../libs/mix.lib.ts/src')
+                }
+            }
         }
-      }
     }
-  }
 };
 
 export default config;
