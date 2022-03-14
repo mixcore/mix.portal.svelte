dfa2268bac4b0f17cba23dc8aba14de6798640ff
phong.cao <phong.cao@orientsoftware.com>
2022-03-14 10:06:36 +0700

    feat(error): add error page

--- apps/mix-portal/src/routes/error/index.svelte
+++ apps/mix-portal/src/routes/error/index.svelte
@@ -1,13 +1,95 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import { Button } from "carbon-components-svelte";
 
-<div class="error-page">
-    <h1>Something wrong, please try again.</h1>
+</script>
+
+<div id="notfound">
+  <div class="notfound">
+    <div class="notfound-404">
+      <h1>404</h1>
+    </div>
+    <h2>WE ARE SORRY, SOMETHING ERROR!</h2>
+    <p>
+      The page you are looking for might have been removed had its name changed
+      or is temporarily unavailable.
+    </p>
+    <div class="notfound-btn">
+        <Button> Try again </Button>
+    </div>
+  </div>
 </div>
 
 <style lang="scss">
-    .error-page {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  #notfound {
+    position: relative;
+    height: 100vh;
+  }
+
+  #notfound .notfound {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .notfound {
+    max-width: 920px;
+    width: 100%;
+    line-height: 1.4;
+    text-align: center;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+
+  .notfound .notfound-404 {
+    position: absolute;
+    height: 100px;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: -1;
+  }
+
+  .notfound .notfound-404 h1 {
+    font-family: 'Maven Pro', sans-serif;
+    color: #ececec;
+    font-weight: 900;
+    font-size: 276px;
+    margin: 0px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .notfound h2 {
+    font-family: 'Maven Pro', sans-serif;
+    font-size: 46px;
+    color: #000;
+    font-weight: 900;
+    text-transform: uppercase;
+    margin: 0px;
+  }
+
+  .notfound p {
+    font-family: 'Maven Pro', sans-serif;
+    font-size: 16px;
+    color: #000;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-top: 15px;
+  }
+
+  .notfound-btn {
+      margin-top: 50px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    .notfound .notfound-404 h1 {
+      font-size: 162px;
     }
+    .notfound h2 {
+      font-size: 26px;
+    }
+  }
 </style>
